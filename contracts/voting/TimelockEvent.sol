53bb5971d01766c948dd1c8f0c69d1f1736b9dd3
brightdev33 <brightdev33@gmail.com>
2020-08-12 09:44:59 +1000

    Implemented timelock, first commit, removed constructor params

---
+++ contracts/voting/TimelockEvent.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract TimelockEvent {
+
+    event NewAdmin(address indexed newAdmin);
+    event NewPendingAdmin(address indexed newPendingAdmin);
+    event NewDelay(uint indexed newDelay);
+    event CancelTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta);
+    event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta);
+    event QueueTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature, bytes data, uint eta);
+    
+}
---
+++ contracts/voting/TimelockStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract TimelockStorage {
+    uint public constant GRACE_PERIOD = 14 days;
+    uint public constant MINIMUM_DELAY = 2 days;
+    uint public constant MAXIMUM_DELAY = 30 days;
+
+    address public admin;
+    address public pendingAdmin;
+    uint public delay;
+
+    mapping (bytes32 => bool) public queuedTransactions;
+}
