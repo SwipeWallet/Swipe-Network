f2712540c4000bc0ace2b5f26a7ce084869f44ec
blockplus <blockplus.dev@gmail.com>
2020-08-17 01:14:49 -0700

    Update voting contract

--- contracts/voting/VotingStorage.sol
+++ contracts/voting/VotingStorage.sol
@@ -21,7 +21,7 @@ contract VotingStorage {
 
     /// @notice The address of the Voting Guardian
     address public _guardian;
-    
+
     address public _authorizedNewGuardian;
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
@@ -115,7 +115,7 @@ contract VotingStorage {
     }
 
     /// @notice The official record of all proposals ever proposed
-    mapping (uint256 => Proposal) public _proposals;
+    mapping (uint256 => Proposal) internal _proposals;
 
     /// @notice The latest proposal for each proposer
     mapping (address => uint256) public _latestProposalIds;
