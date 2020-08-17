60bbde3c9dc720ca493c70abcb2b245daaeb0420
blockplus <blockplus.dev@gmail.com>
2020-08-17 13:08:32 -0700

    Integrate staking checkpoint

--- contracts/staking/IStaking.sol
+++ contracts/staking/IStaking.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
 
 interface IStaking {
-    function _stakedMap(address account) external view returns (uint256);
+    function getPriorStakedAmount(address staker, uint256 blockNumber) external view returns (uint256);
 }
